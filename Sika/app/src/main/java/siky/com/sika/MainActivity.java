4ddcb04fc8ef11c15dca2b74a595c1d9fc3b52be
Ananthakrishna <bhaskarananthakrishna@gmail.com>
2016-08-26 00:00:19 +0530

    log for onCreate

--- Sika/app/src/main/java/siky/com/sika/MainActivity.java
+++ Sika/app/src/main/java/siky/com/sika/MainActivity.java
@@ -3,6 +3,7 @@ package siky.com.sika;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("main activity","oncreate");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
